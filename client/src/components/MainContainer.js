01dff3007ddfb0218b4ddc7f4d5eade2e2c9f854
unknown <kylestauch99@gmail.com>
2018-11-19 21:25:50 -0600

    Added container & home container

---
+++ client/src/components/MainContainer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import Home from './Home';
+
+const MainContainer = () => (
+    <React.Fragment>
+        <Home />
+    </React.Fragment>
+);
+
+export default MainContainer;
