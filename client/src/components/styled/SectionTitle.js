3a2c1d99672cc730fbbeb7274743634db038e5e5
unknown <kylestauch99@gmail.com>
2018-11-24 10:32:57 -0600

    Finished SectionTitle component

---
+++ client/src/components/styled/SectionTitle.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const SectionTitle = ({ text }) => (
+    <h1 style={{fontFamily: 'Reem Kufi, sans-serif', fontSize: '2rem'}}>{text}</h1>
+);
+
+export default SectionTitle;
