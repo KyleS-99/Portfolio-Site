fa58282597b9ed672fe814baa74e12f48a141066
unknown <kylestauch99@gmail.com>
2018-12-12 17:50:51 -0600

    Created contact component

---
+++ client/src/components/Contact.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import styled from 'styled-components';
+
+class Contact extends Component {
+    state = {
+        
+    }
+    render() {
+        return (
+            <div></div>
+        );
+    }
+}
+
+export default Contact;
