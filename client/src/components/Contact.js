c11ea9ce4aa980b76bb406c07413728db73cf8d0
unknown <kylestauch99@gmail.com>
2018-12-15 17:57:53 -0600

    Changed styles and html/jsx

--- client/src/components/Contact.js
+++ client/src/components/Contact.js
@@ -11,16 +11,44 @@ const ContactContainer = styled.div`
 
 const FormContainer = styled.div`
     width: 80%;
-    max-width: 
+    max-width: 800px;
+    margin: 0 auto;
+`;
+
+const Form = styled.form`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+`;
+
+const LabelContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+    flex: 1;
+`;
+
+const InputContainer = styled.div`
+    display: flex;
+    flex-direction: row;
+    width: 100%;
+
+    & > div + div {
+        margin-right: 20px;
+    }
 `;
 
 const Input = styled.input`
-
+    
 `;
 
-const TextArea = styled.textarea``
+const TextArea = styled.textarea`
+    width: 100%;
+`;
 
-;
+const Label = styled.label`
+    text-transform: uppercase;
+`;
 
 class Contact extends Component {
     state = {
@@ -39,32 +67,55 @@ class Contact extends Component {
             <ContactContainer>
                 <SectionTitle text="Contact" />
 
-                <form action="/email" method="post" autoComplete="off">
-                    <Input 
-                        onChange={this.onChange} 
-                        placeholder="Name" 
-                        value={name} 
-                        name="name"
-                    />
-                    <Input 
-                        onChange={this.onChange} 
-                        placeholder="Subject" 
-                        value={email} 
-                        name="subject"
-                    />
-                    <Input 
-                        onChange={this.onChange} 
-                        style={{ display: 'none' }} 
-                        value={hiddenInput} 
-                        name="hiddenInput"
-                    />
-                    <TextArea 
-                        onChange={this.onChange} 
-                        placeholder="Message" 
-                        value={message} 
-                        name="message" 
-                    />
-                </form>
+                <FormContainer>
+                    <Form autoComplete="off">
+                        <div style={{ width: '100%' }}>
+                            <InputContainer>
+                                <LabelContainer>
+                                    <Label htmlFor="name">name</Label>
+                                    <Input 
+                                        onChange={this.onChange} 
+                                        placeholder="Name" 
+                                        value={name} 
+                                        name="name"
+                                        id="name"
+                                    />
+                                </LabelContainer>
+
+                                <LabelContainer>
+                                    <Label htmlFor="email">email</Label>
+                                    <Input 
+                                        onChange={this.onChange} 
+                                        placeholder="Subject" 
+                                        value={email} 
+                                        name="subject"
+                                        id="email"
+                                    />
+                                </LabelContainer>
+                            
+                                <Input 
+                                    onChange={this.onChange} 
+                                    style={{ display: 'none' }} 
+                                    value={hiddenInput} 
+                                    name="hiddenInput"
+                                />
+                            </InputContainer>
+                        </div>
+                        
+                        <div style={{ width: '100%' }}>
+                            <LabelContainer>
+                                <Label htmlFor="message">message</Label>
+                                <TextArea 
+                                    onChange={this.onChange} 
+                                    placeholder="Message" 
+                                    value={message} 
+                                    name="message" 
+                                    id="message"
+                                />
+                            </LabelContainer>
+                        </div>
+                    </Form>
+                </FormContainer>
             </ContactContainer>
         );
     }
