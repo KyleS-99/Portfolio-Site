134367403269419ab11bbb61e5090344334ba69f
unknown <kylestauch99@gmail.com>
2020-01-19 07:53:37 -0600

    Updated package.json, changed box shadow, and added custom hook

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,13 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './App.css';
 import 'normalize.css';
 
 import MainContainer from './components/MainContainer';
 
-class App extends Component {
-    render() {
-        return (
-            <MainContainer />
-        );
-    }
+const App = () => {
+    return (
+        <MainContainer />
+    );
 }
 
 export default App;
