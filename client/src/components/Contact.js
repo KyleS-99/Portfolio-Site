d66e39773c2a0490e50b0087f900803fede47587
unknown <kylestauch99@gmail.com>
2020-01-21 16:09:11 -0600

    Extracted form logic to its own component and created GoBack component for when form is submitted.

--- client/src/components/Contact.js
+++ client/src/components/Contact.js
@@ -1,11 +1,9 @@
 import React, { useState } from 'react';
 import styled from 'styled-components';
-import axios from 'axios';
 
 import SectionTitle from './styled/SectionTitle';
-import Error from './styled/Error';
-import Spinner from './styled/Spinner';
-import useInput from './hooks/useInput';
+import Form from './Form';
+import GoBack from './GoBack';
 
 const ContactContainer = styled.div`
     width: 100%;
@@ -13,214 +11,14 @@ const ContactContainer = styled.div`
     position: relative;
 `;
 
-const FormContainer = styled.div`
-    width: 80%;
-    max-width: 800px;
-    margin: 0 auto;
-`;
-
-const Form = styled.form`
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-    ${props => props.submitted && `display: none;`}
-
-    & > div + div {
-        margin-top: 37px;
-    }
-
-    @media (max-width: 768px) {
-        & > div + div {
-            margin-top: 1rem;
-        }
-    }
-`;
-
-const LabelContainer = styled.div`
-    display: flex;
-    flex-direction: column;
-    flex: 1;
-
-    & > input, textarea {
-        outline: none;
-        border: 1px solid #eee;
-        font-size: 18px;
-    }
-`;
-
-const InputContainer = styled.div`
-    display: flex;
-    flex-direction: row;
-    width: 100%;
-
-    & > div + div {
-        margin-left: 30px;
-    }
-
-    @media (max-width: 768px) {
-        flex-direction: column;
-
-        & > div + div {
-            margin: 0px;
-            margin-top: 1rem;
-        }
-    }
-`;
-
-const Input = styled.input`
-    padding: 10px;
-
-    ${props => props.error !== undefined ? `border: 1px solid #cc0000 !important;` : ''}
-`;
-
-const TextArea = styled.textarea`
-    width: 100%;
-    box-sizing: border-box;
-    padding: 10px;
-    resize: vertical;
-
-    ${props => props.error !== undefined ? `border: 1px solid #cc0000 !important;` : ''}
-`;
-
-const Label = styled.label`
-    text-transform: uppercase;
-    font-size: 20px;
-    margin-bottom: 3px;
-`;
-
-const Submit = styled.button`
-    width: 150px;
-    height: 40px;
-    background: #000;
-    border: none;
-    outline: none;
-    border-radius: 50px;
-    color: #fff;
-    text-transform: capitalize;
-    font-weight: 300;
-    -webkit-transition: .2s;
-    transition: .2s;
-    text-align: center;
-    line-height: 40px;
-    font-size: 1.1rem;
-    margin-top: 3rem;
-    box-shadow: 0px 8px 15px rgba(0, 0, 0, 0.5);
-
-    &:hover {
-        cursor: pointer;
-        transform: translateY(-2px);
-    }
-
-    &:active {
-        transform: translateY(4px);
-        transform: scale(0.9);
-    }
-`;
-
 const Contact = () => {
-    const [email, bindEmail, resetEmail] = useInput('');
-    const [name, bindName, resetName] = useInput('');
-    const [message, bindMessage, resetMessage] = useInput('');
-    const [hiddenInput, bindHiddenInput, resetHiddenInput] = useInput('');
-    const [errors, setErrors] = useState({});
     const [formSubmitted, setFormSubmitted] = useState(false);
-
-    const onSubmit = (e) => {
-        e.preventDefault();
-
-        // Prevent bot spam
-        if (hiddenInput === '') {
-            axios.post('/api/email', { email, name, message })
-                .then(() => {
-                    setFormSubmitted(true);
-                    resetEmail();
-                    resetName();
-                    resetMessage();
-                    resetHiddenInput();
-                    setErrors({});
-                })
-                .catch(err => {
-                    setErrors(err.response.data);
-                });
-        }
-    }
 
     return (
         <ContactContainer id="contact">
             <SectionTitle text="Contact" />
 
-            <FormContainer>
-                <Form 
-                    autoComplete="off" 
-                    onSubmit={onSubmit}
-                    submitted={formSubmitted}
-                >
-                    <div style={{ width: '100%' }}>
-                        <InputContainer>
-                            <LabelContainer>
-                                <Label htmlFor="name">name</Label>
-                                <Input 
-                                    {...bindName}
-                                    placeholder="Jon Snow" 
-                                    type="text"
-                                    name="name"
-                                    id="name"
-                                    error={errors.name}
-                                />
-                                {
-                                    errors.name && <Error error={errors.name} />
-                                }
-                            </LabelContainer>
-
-                            <LabelContainer>
-                                <Label htmlFor="email">email</Label>
-                                <Input 
-                                    {...bindEmail}
-                                    placeholder="jon@snow.com" 
-                                    type="email"
-                                    name="email"
-                                    id="email"
-                                    error={errors.email}
-                                />
-                                {
-                                    errors.email && <Error error={errors.email} />
-                                }
-                            </LabelContainer>
-                        
-                            <Input 
-                                {...bindHiddenInput}
-                                style={{ display: 'none' }} 
-                                type="text"
-                                name="hiddenInput"
-                            />
-                        </InputContainer>
-                    </div>
-                    
-                    <div style={{ width: '100%' }}>
-                        <LabelContainer>
-                            <Label htmlFor="message">message</Label>
-                            <TextArea 
-                                {...bindMessage}
-                                placeholder="Winter is coming... Hurry send your fastest raven!" 
-                                name="message" 
-                                id="message"
-                                rows="11"
-                                error={errors.message}
-                            />
-                            {
-                                errors.message && <Error error={errors.message} />
-                            }
-                        </LabelContainer>
-                    </div>
-
-                    <Submit type="submit">
-                        send
-                    </Submit>
-                </Form>
-
-                { formSubmitted && <Spinner /> }
-            </FormContainer>
+            { formSubmitted ? <GoBack /> : <Form formSubmitted={formSubmitted} setFormSubmitted={setFormSubmitted} /> }
         </ContactContainer>
     );
 }
---
+++ client/src/components/Form.js
@@ -0,0 +1,216 @@
+import React, { useState } from 'react';
+import styled from 'styled-components';
+import axios from 'axios';
+
+import Error from './styled/Error';
+import Spinner from './styled/Spinner';
+import useInput from './hooks/useInput';
+
+const FormContainer = styled.div`
+    width: 80%;
+    max-width: 800px;
+    margin: 0 auto;
+`;
+
+const ContactForm = styled.form`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    ${props => props.submitted && `display: none;`}
+
+    & > div + div {
+        margin-top: 37px;
+    }
+
+    @media (max-width: 768px) {
+        & > div + div {
+            margin-top: 1rem;
+        }
+    }
+`;
+
+const LabelContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+    flex: 1;
+
+    & > input, textarea {
+        outline: none;
+        border: 1px solid #eee;
+        font-size: 18px;
+    }
+`;
+
+const InputContainer = styled.div`
+    display: flex;
+    flex-direction: row;
+    width: 100%;
+
+    & > div + div {
+        margin-left: 30px;
+    }
+
+    @media (max-width: 768px) {
+        flex-direction: column;
+
+        & > div + div {
+            margin: 0px;
+            margin-top: 1rem;
+        }
+    }
+`;
+
+const Input = styled.input`
+    padding: 10px;
+
+    ${props => props.error !== undefined ? `border: 1px solid #cc0000 !important;` : ''}
+`;
+
+const TextArea = styled.textarea`
+    width: 100%;
+    box-sizing: border-box;
+    padding: 10px;
+    resize: vertical;
+
+    ${props => props.error !== undefined ? `border: 1px solid #cc0000 !important;` : ''}
+`;
+
+const Label = styled.label`
+    text-transform: uppercase;
+    font-size: 20px;
+    margin-bottom: 3px;
+`;
+
+const Submit = styled.button`
+    width: 150px;
+    height: 40px;
+    background: #000;
+    border: none;
+    outline: none;
+    border-radius: 50px;
+    color: #fff;
+    text-transform: capitalize;
+    font-weight: 300;
+    -webkit-transition: .2s;
+    transition: .2s;
+    text-align: center;
+    line-height: 40px;
+    font-size: 1.1rem;
+    margin-top: 3rem;
+    box-shadow: 0px 8px 15px rgba(0, 0, 0, 0.5);
+
+    &:hover {
+        cursor: pointer;
+        transform: translateY(-2px);
+    }
+
+    &:active {
+        transform: translateY(4px);
+        transform: scale(0.9);
+    }
+`;
+
+const Form = (props) => {
+    const [email, bindEmail, resetEmail] = useInput('');
+    const [name, bindName, resetName] = useInput('');
+    const [message, bindMessage, resetMessage] = useInput('');
+    const [hiddenInput, bindHiddenInput, resetHiddenInput] = useInput('');
+    const [errors, setErrors] = useState({});
+
+    const onSubmit = (e) => {
+        e.preventDefault();
+
+        // Prevent bot spam
+        if (hiddenInput === '') {
+            axios.post('/api/email', { email, name, message })
+                .then(() => {
+                    this.props.setFormSubmitted(true);
+                    resetEmail();
+                    resetName();
+                    resetMessage();
+                    resetHiddenInput();
+                    setErrors({});
+                })
+                .catch(err => {
+                    setErrors(err.response.data);
+                });
+        }
+    }
+
+    return (
+        <FormContainer>
+            <ContactForm
+                autoComplete="off" 
+                onSubmit={onSubmit}
+                submitted={props.formSubmitted}
+            >
+                <div style={{ width: '100%' }}>
+                    <InputContainer>
+                        <LabelContainer>
+                            <Label htmlFor="name">name</Label>
+                            <Input 
+                                {...bindName}
+                                placeholder="Jon Snow" 
+                                type="text"
+                                name="name"
+                                id="name"
+                                error={errors.name}
+                            />
+                            {
+                                errors.name && <Error error={errors.name} />
+                            }
+                        </LabelContainer>
+
+                        <LabelContainer>
+                            <Label htmlFor="email">email</Label>
+                            <Input 
+                                {...bindEmail}
+                                placeholder="jon@snow.com" 
+                                type="email"
+                                name="email"
+                                id="email"
+                                error={errors.email}
+                            />
+                            {
+                                errors.email && <Error error={errors.email} />
+                            }
+                        </LabelContainer>
+                    
+                        <Input 
+                            {...bindHiddenInput}
+                            style={{ display: 'none' }} 
+                            type="text"
+                            name="hiddenInput"
+                        />
+                    </InputContainer>
+                </div>
+                
+                <div style={{ width: '100%' }}>
+                    <LabelContainer>
+                        <Label htmlFor="message">message</Label>
+                        <TextArea 
+                            {...bindMessage}
+                            placeholder="Winter is coming... Hurry send your fastest raven!" 
+                            name="message" 
+                            id="message"
+                            rows="11"
+                            error={errors.message}
+                        />
+                        {
+                            errors.message && <Error error={errors.message} />
+                        }
+                    </LabelContainer>
+                </div>
+
+                <Submit type="submit">
+                    send
+                </Submit>
+            </ContactForm>
+
+            { props.formSubmitted && <Spinner /> }
+        </FormContainer>
+    );
+};
+
+export default Form;
